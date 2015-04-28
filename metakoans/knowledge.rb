bde3bbeccb2eadaba5be8e6beb8d86c784116207
George Zhukov <scofield.cross@gmail.com>
2015-04-29 02:14:00 +0300

    finished knowledge.rb

---
+++ metakoans/knowledge.rb
@@ -0,0 +1,24 @@
+class Module
+  def attribute(name, &block)
+    # Cranking hash
+    return name.map {|key, value| attribute(key) {value}} if name.kind_of?(Hash)
+
+    class_eval do
+      attr_writer name.to_sym
+
+      define_method (name + '?') do
+        send(name)? true : false
+      end
+
+      # Attribute getter
+      define_method (name) do
+        if instance_variable_defined?('@' + name)
+          instance_variable_get('@' + name)
+        elsif block_given?          
+          instance_eval(&block)
+        end
+      end
+
+    end
+  end
+end
